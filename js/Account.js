3c00c6e29ba9d8875f75f031d2130417781961b9
tran doan triet <triet.tran@alpaca.vn>
2023-08-17 21:53:19 +0700

    a

--- js/Account.js
+++ js/Account.js
@@ -1,6 +1,7 @@
 
 let popUp = document.getElementById('popup-account-info')
-
+let popUpWidraw = document.getElementById('popup-account-widraw')
+let popUpWidrawSuccess = document.getElementById('popup-account-widraw-success')
 function openPopup(){
     popUp.style.visibility = 'visible'
     popUp.style.top = '50%'
@@ -11,3 +12,19 @@ function closePopup(){
     popUp.style.top = '0'
     popUp.style.transform = 'translate(-50%,-50%) scale(0.1)'
 }
+function openPopupWidraw(){
+    popUpWidraw.style.visibility = 'visible'
+    popUpWidraw.style.top = '50%'
+    popUpWidraw.style.transform = 'translate(-50%,-50%) scale(1)'
+}
+function closePopupWidraw(){
+    popUpWidraw.style.visibility = 'hidden'
+    popUpWidraw.style.top = '0'
+    popUpWidraw.style.transform = 'translate(-50%,-50%) scale(0.1)'
+    popUpWidrawSuccess.style.display = 'block'
+}
+
+function closePopupWidrawSuccess(){
+    popUpWidrawSuccess.style.display = 'none'
+}
+
